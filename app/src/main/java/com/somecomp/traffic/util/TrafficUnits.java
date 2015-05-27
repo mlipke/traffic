c88e5564c1f3e8c0c735f9379d4973cf3037aea6
Matthes Lipke <matthes.lipke@mailbox.tu-dresden.de>
2015-05-27 12:00:59 +0200

    Removed parse method from TrafficUnits

--- app/src/main/java/com/somecomp/traffic/util/TrafficUnits.java
+++ app/src/main/java/com/somecomp/traffic/util/TrafficUnits.java
@@ -5,24 +5,6 @@ import java.math.RoundingMode;
 
 public class TrafficUnits {
 
-    public static float parse(String str) {
-        float value = 0.0f;
-        String[] split = str.split(" ");
-
-        switch (split[1]) {
-            case "KiB":
-                value = Float.parseFloat(split[0]) / 1024.0f;
-                break;
-            case "MiB":
-                value = Float.parseFloat(split[0]);
-                break;
-            case "GiB":
-                value = Float.parseFloat(split[0]) * 1024.0f;
-        }
-
-        return value;
-    }
-
     public static String getString(double amount) {
         if (amount < 1) { return String.valueOf(round(amount * 1024, 2)) + " KiB"; }
         else if (amount <= 1024) { return String.valueOf(round(amount, 2)) + " MiB"; }
