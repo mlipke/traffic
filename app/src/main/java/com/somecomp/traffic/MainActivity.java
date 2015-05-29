66a7efadbb735da8897d9cb92a48ef9012b03730
Ian List <ian_alexander.list@mailbox.tu-dresden.de>
2015-05-29 18:52:23 +0200

    Hid the 'Menu icon' while the settings don't do anything

--- app/src/main/java/com/somecomp/traffic/MainActivity.java
+++ app/src/main/java/com/somecomp/traffic/MainActivity.java
@@ -36,7 +36,9 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        //Commented this, so that the Menu is hidden, while it doesn't do anything.
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
